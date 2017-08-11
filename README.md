8b83659197244feee6ca75366f5ee85fb8f33cf5
Rashin Sergey <ser@rashin.me>
2017-08-11 22:47:46 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+test-bloc.loc
+=============
+
+A Symfony project created on August 10, 2017, 8:59 pm.
---
+++ docker/init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# init.sh
+
+chmod 777 /var/www/docker/.env
+cat /var/www/docker/.env >> /root/.bashrc
+
+cd /var/www/
+
+composer install --prefer-source --no-interaction
+
+echo db:5432:blog:dev:dev > ~/.pgpass
+chmod 0600 ~/.pgpass
+
+until psql -h "db" -U "dev" -d "blog"; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console cache:warmup --env=dev
+php bin/console cache:warmup --env=prod
+
+chown -R www-data:www-data ./var
+
+php-fpm -F
